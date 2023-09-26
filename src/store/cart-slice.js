3a3a4a745d5284980d85017e678232aa23632136
sumitmehta021 <136010191+sumitmehta021@users.noreply.github.com>
2023-09-26 13:44:57 +0530

    final cart with redux, useEffect, http-request, thunks

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,15 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { items: [], totalQuantity: 0 };
+const initialState = { items: [], totalQuantity: 0, changed: false };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+      state.changed = false;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -26,6 +33,7 @@ const cartSlice = createSlice({
 
     removeItemFromCart(state, action) {
       state.totalQuantity--;
+      state.changed = true;
       const existingItem = state.items.find(
         (item) => item.id === action.payload
       );
@@ -34,6 +42,7 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== action.payload);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
       }
     }
   }
